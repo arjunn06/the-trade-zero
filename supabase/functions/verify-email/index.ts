4e67cb3b06f0757d44099baadb5cbae534ab107c
arjunn06 <53135341+arjunn06@users.noreply.github.com>
2025-08-06 15:15:05 +0530

    Update index.ts

--- supabase/functions/verify-email/index.ts
+++ supabase/functions/verify-email/index.ts
@@ -24,7 +24,7 @@ serve(async (req) => {
     Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!,
     {
       global: {
-        headers: { Authorization: `Bearer ${token}` }
+        headers: { Authorization: `Bearer ${session.access_token}` }
       }
     }
   );
